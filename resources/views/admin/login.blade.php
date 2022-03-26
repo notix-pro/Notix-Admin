35b007ad8c950b50a88fb5a13740852b33ebd791
Ourmine-Onyx <ogtv.dev@gmail.com>
2022-03-26 22:29:24 +0100

    Update login.blade.php

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,7 +5,7 @@
         <h1>Wrapper Login</h1>
         <x-form.inputs.input inputId="Input Id" inputName="Input Name" inputPlaceholder="Input Placeholder" labelText="Label Text" />
         <br><br>
-        <x-form.select selectId="Select Id" selectName="Select Name" selectPlaceholder="Select Placeholder" labelText="Label Text"
+        <x-form.select selectId="Select Id" selectName="Select Name" labelText="Label Text"
             :options="['' => 'Wybierz','option1' => 'Option 1', 'option2' => 'Option 2']"/>
 
     </div>

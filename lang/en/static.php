533ca9692feb8a9f6039b9e945c6268ce5099f38
Ourmine-Onyx <ogtv.dev@gmail.com>
2022-03-26 23:21:20 +0100

    Update static.php

---
+++ lang/en/static.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Static Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'site_locale' => 'pl',
+    'site_name' => 'NOTIX Admin',
+    'description' => 'NOTIX Admin is a simple admin panel for laravel sites.',
+
+];

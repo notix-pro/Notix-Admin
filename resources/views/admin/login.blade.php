34a2cdd72afab91011c256fbc723d1e4ce94ccf6
Ourmine-Onyx <ogtv.dev@gmail.com>
2022-03-26 22:14:24 +0100

    Update login.blade.php

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -6,7 +6,7 @@
         <x-form.inputs.input inputId="Input Id" inputName="Input Name" inputPlaceholder="Input Placeholder" labelText="Label Text" />
         <br><br>
         <x-form.select selectId="Select Id" selectName="Select Name" selectPlaceholder="Select Placeholder" labelText="Label Text"
-            :options="['' => 'Wybierz','option1' => 'Option 1', 'option2' => 'Option 2']" />
+            :options="['' => 'Wybierz','option1' => 'Option 1', 'option2' => 'Option 2']"/>
 
     </div>
 @endsection
